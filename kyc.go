e08e560cc6b0889f3e599b4cb50a5a01576f8f6d
Kanikamalik <k.malik612@gmail.com>
2017-04-22 14:28:26 -0400

    c

--- kyc.go
+++ kyc.go
@@ -103,6 +103,10 @@ func (t *SimpleChaincode) create_kyc(stub shim.ChaincodeStubInterface, k []strin
 	kyc_id         := "\"KYC_Id\":\""+k[1]+"\", "							// Variables to define the JSON
 	kyc_type       := "\"Kyc_Type\"\""+k[2]+"\","
     cust_id        := "\"Kyc_Type\"\""+k[0]+"\","
+
+	fmt.Printf(+k[0]);
+	fmt.Printf(+k[1]);
+	fmt.Printf(+k[2]);
 	kyc_json := "{"+kyc_id+kyc_type+cust_id+"}" 	// Concatenates the variables to create the total JSON object
 
 	logger.Debug("kyc_json: ", kyc_json)
