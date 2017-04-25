b6053898dae3faef6765cac743e2c5fc5d108fd3
Kanikamalik <k.malik612@gmail.com>
2017-04-25 08:09:24 -0400

    nbcz,m

--- kyc.go
+++ kyc.go
@@ -177,10 +177,10 @@ func (t *SimpleChaincode) get_kyc_details(stub shim.ChaincodeStubInterface, cust
 	var v KYCInfo
 
 	for _, id := range kycs.KYCs {
-
+		
 		v, err = t.retrieve_v5c(stub, id)
 
-		if err != nil {return nil, errors.New("Failed to retrieve V5C")}
+		if err != nil {return nil, errors.New("Failed to retrieve kyc" +kyc_id)}
 temp, err = t.get_kyc(stub, v,cust_id)
 
 		if err == nil {
